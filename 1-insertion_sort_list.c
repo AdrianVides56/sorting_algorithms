9218dc1032e77dd3ddd6f15e2d1e43f282146528
AdrianVides56 <adrianvj18@gmail.com>
2021-02-01 23:56:57 -0500

    Changed functions of place

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,6 +1,29 @@
 #include "sort.h"
 
 /**
+ * swap_nodes - swap node1 and node2 places
+ * @list: linked list
+ * @node1: current node
+ * @node2: next node to current
+ */
+void swap_nodes(listint_t **list, listint_t **node1, listint_t **node2)
+{
+	listint_t *aux = (*node1)->prev;
+
+	(*node1)->next = (*node2)->next;
+
+	if ((*node2)->next != NULL)
+		(*node2)->next->prev = (*node1);
+	(*node1)->prev = (*node2);
+	(*node2)->next = (*node1);
+	(*node2)->prev = aux;
+	if ((*node2)->prev != NULL)
+		(*node2)->prev->next = (*node2);
+	else
+		(*list) = (*node2);
+}
+
+/**
  * insertion_sort_list - sorts a doubly linked list
  * using the insertion sort algorithm
  * @list: list to sort
@@ -28,27 +51,4 @@ void insertion_sort_list(listint_t **list)
 		}
 		current = current->next;
 	}
-}
-
-/**
- * swap_nodes - swap node1 and node2 places
- * @list: linked list
- * @node1: current node
- * @node2: next node to current
- */
-void swap_nodes(listint_t **list, listint_t **node1, listint_t **node2)
-{
-	listint_t *aux = (*node1)->prev;
-
-	(*node1)->next = (*node2)->next;
-
-	if ((*node2)->next != NULL)
-		(*node2)->next->prev = (*node1);
-	(*node1)->prev = (*node2);
-	(*node2)->next = (*node1);
-	(*node2)->prev = aux;
-	if ((*node2)->prev != NULL)
-		(*node2)->prev->next = (*node2);
-	else
-		(*list) = (*node2);
 }
--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,39 +1,6 @@
 #include "sort.h"
 
 /**
- * selection_sort - sort an array with selection sort algorithm
- * @array: array to sort
- * @size: size of array
- */
-void selection_sort(int *array, size_t size)
-{
-	int current = 0, tmp = 0, check = 0;
-	size_t i = 0, j = 0, sidx = 0;
-
-	for (i = 0; i < size; i++)
-	{
-		current = array[i];
-		check = 0;
-		tmp = max_int(array, size);
-		for (j = i; j < size; j++)
-		{
-			if (array[j] < array[i] && array[j] < tmp)
-			{
-				tmp = array[j];
-				check = 1;
-				sidx = j;
-			}
-		}
-		if (check == 1)
-		{
-			array[i] = tmp;
-			array[sidx] = current;
-			print_array(array, size);
-		}
-	}
-}
-
-/**
  * max_int - finds the greater number in an array
  * @array: array
  * @size: size of the array
@@ -50,4 +17,38 @@ int max_int(int *array, size_t size)
 			max = array[idx];
 	}
 	return (max);
+}
+
+/**
+ * selection_sort - sort an array with selection sort algorithm
+ * @array: array to sort
+ * @size: size of array
+ */
+void selection_sort(int *array, size_t size)
+{
+	int current = 0, tmp = 0, check = 0;
+	size_t i = 0, j = 0, sidx = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		current = array[i];
+		check = 0;
+		tmp = max_int(array, size);
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[i] && array[j] < tmp)
+			{
+				tmp = array[j];
+				check = 1;
+				sidx = j;
+			}
+		}
+		if (check == 1)
+		{
+			/*printf("sidx: %d -", array[sidx]);*/
+			array[i] = tmp;
+			array[sidx] = current;
+			print_array(array, size);
+		}
+	}
 }
